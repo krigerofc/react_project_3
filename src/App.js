89e110aee7878a5888874f273de285aba5ba16ce
krigerofc <pedronovogamer@gmail.com>
2024-03-14 14:22:46 -0300

    componentes funcionais - footer e icone para page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 //import './app.cs'
-
 import Header from "./components/Header";
+import Rodape from "./components/Rodape";
 
 
 function App() {
   return (
     <> 
-    <Header/>
-    <h1>hello</h1>
-    <p>OIIII</p>
+      <Header/>
+      <h1>hello</h1>
+      <p>OIIII</p>
+      <Rodape/>
     </>
   );
 }
---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css'
+
+function Rodape(){
+    return(
+        <footer className={styles.footer}>
+            <h2>Terceirão ADS &copy; Desenvolvido por Pedro Henrique 2024</h2>
+        </footer>
+    );
+}
+
+export default Rodape;
