9667c80d0859f6283cab5f2aedf2fa378ff4a2ba
krigerofc <pedronovogamer@gmail.com>
2024-03-20 12:55:16 -0300

    refatoração, export e import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 //import './app.cs'
 import Banner from "./components/Banner";
 import Card from "./components/Card";
-import Category from "./components/Category";
+import Category, {categorias, filtrerCategory} from "./components/Category";
 import Container from "./components/Container";
 import Header from "./components/Header";
 import Rodape from "./components/Rodape";
-
-import videos from './json/videos.json'//importar json
-
-const categorias = [
-  'Geografia',
-  'Como fazer e usar',
-  'Astronomia e Geografia',
-  'Climatologia, meteorologia, vegetação',
-  'Geologia e Hidrografia',
-]
-function filtrerCategory(id){
-  return videos.filter(video => video.category === categorias[id])
-}
-
 
 function App() {
   return (
@@ -26,13 +12,11 @@ function App() {
       <Header/>
       <Banner image='wallpaper_pcgreen'  />
       <Container>
-        <Category category='Geografia'>
-          {filtrerCategory(0).map((video) => <Card id={video.id} key={video.id}/>)}
+        {categorias.map((category, index) => 
+        <Category category={category}>
+          {filtrerCategory(index).map((video) => <Card id={video.id} key={video.id}/>)}
         </Category>
-
-        <Category category='Como fazer e usar'>
-          {filtrerCategory(1).map((video) => <Card id={video.id} key={video.id}/>)}
-        </Category>
+        )}
       </Container>
       <Rodape/>
     </>
@@ -45,4 +29,7 @@ function App() {
 // aula 5
 //tudo que é JAVASCRIPT BOTA EM {}
 //refatorar codigo - Rescreever e reduzir linhas
+
+// aula 7
+// refatoração de codigo e evitando de repetir, arrumando mapeamento
 export default App;
--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,6 +1,18 @@
 import styles from './Category.module.css';
+import videos from '../../json/videos.json'
 
+export const categorias = [
+    'Geografia',
+    'Como fazer e usar',
+    'Astronomia e Geografia',
+    'Climatologia, meteorologia, vegetação',
+    'Geologia e Hidrografia',
+  ]
 
+export function filtrerCategory(id){
+    return videos.filter(video => video.category === categorias[id])
+}
+  
 function Category({ category, children }){
     return(
     <section className={styles.category}>
